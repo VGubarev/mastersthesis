bb46128e23ba406339e000a7b154b31b190f2c5e
Vladimir Gubarev <vladimir.gubarev@techstories.ru>
2020-05-12 16:05:11 +0300

    Fix experimental chapter data, write and fix text

--- src/python/analyze_deltas.py
+++ src/python/analyze_deltas.py
@@ -8,7 +8,7 @@ import datetime
 from scipy.stats import sem, t
 from scipy import mean
 
-f = open('data/spin/reduced/order.out', 'r')
+f = open('data/spin/46aa2776-91e9-11ea-8090-8a41a21ee60e/order.out', 'r')
 f1 = f.readlines()
 
 raw_deltas = []
@@ -18,39 +18,48 @@ for line in f1:
     now = datetime.datetime.strptime(line,"%Y-%m-%d %H:%M:%S.%f UTC\n").timestamp()
     if (now - prev > 5):
         continue
-    # output = f"{now - prev:.9f}"
-    # print (output)
+    raw_deltas.append(now - prev)
+    prev = now
+
+f = open('data/spin/af5b4516-91e9-11ea-a2d0-5862c1a46ee4/order.out', 'r')
+f1 = f.readlines()
+
+prev = datetime.datetime.strptime(f1[0],"%Y-%m-%d %H:%M:%S.%f UTC\n").timestamp()
+f1 = f1[1:]
+for line in f1:
+    now = datetime.datetime.strptime(line,"%Y-%m-%d %H:%M:%S.%f UTC\n").timestamp()
+    if (now - prev > 5):
+        continue
     raw_deltas.append(now - prev)
     prev = now
 
 deltas = np.array(raw_deltas)
 
-low, high = np.percentile(deltas[deltas > 0.001], [2.5, 97.5])
+low, high = np.percentile(deltas[deltas > 0.005], [2.5, 97.5])
 mean = low + (high - low) / 2
 delta = mean - low
 print (low)
 print (high)
-print (np.min(deltas[deltas > 0.001]))
-print (np.max(deltas[deltas > 0.001]))
+print (np.min(deltas[deltas > 0.005]))
+print (np.max(deltas[deltas > 0.005]))
 print ("Signal TCP latency " + str(mean) + " ± " + str(delta))
-print (len(deltas[(deltas >= low) & (deltas <= high)])/len(deltas[deltas > 0.001]))
+print (len(deltas[(deltas >= low) & (deltas <= high)])/len(deltas[deltas > 0.005]))
 
-low2, high2 = np.percentile(deltas[deltas <= 0.001], [2.5, 97.5])
+low2, high2 = np.percentile(deltas[deltas <= 0.005], [2.5, 97.5])
 mean2 = low2 + (high2 - low2) / 2
 delta = mean2 - low2
 print (low2)
 print (high2)
-print (np.min(deltas[deltas <= 0.001]))
-print (np.max(deltas[deltas <= 0.001]))
+print (np.min(deltas[deltas <= 0.005]))
+print (np.max(deltas[deltas <= 0.005]))
 print ("Signal TCP latency " + str(mean2) + " ± " + str(delta))
-print (len(deltas[(deltas >= low2) & (deltas <= high2)])/len(deltas[deltas <= 0.001]))
+print (len(deltas[(deltas >= low2) & (deltas <= high2)])/len(deltas[deltas <= 0.005]))
 
-print (len(deltas[(deltas >= 0.000120) & (deltas <= 0.001)]))
-print (len(deltas[deltas <= 0.001]))
-print (len(deltas[deltas > 0.001]))
+# print (len(deltas[(deltas >= 0.000120) & (deltas <= 0.005)]))
+print (len(deltas[deltas <= 0.005]))
+print (len(deltas[deltas > 0.005]))
 
 fig, ax = plt.subplots()
-sns.distplot(deltas[deltas <= 0.001], kde=False, norm_hist=True, hist_kws={'edgecolor':'black'}, color='#3782CC')
-sns.distplot(deltas[deltas > 0.001], kde=False, norm_hist=True, hist_kws={'edgecolor':'black'}, color='#3782CC')
-# sns.distplot(deltas, kde=False, norm_hist=True, hist_kws={'edgecolor':'black'}, color='#3782CC')
+sns.distplot(deltas[deltas <= 0.005], kde=False, norm_hist=True, hist_kws={'edgecolor':'black'}, color='#3782CC')
+sns.distplot(deltas[deltas > 0.005], kde=False, norm_hist=True, hist_kws={'edgecolor':'black'}, color='#3782CC')
 plt.show()
